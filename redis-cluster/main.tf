2d1132b25e2d0063733651bd087b688dc46a463e
Navneet Shahi <navneetshahi345@gmail.com>
2025-03-11 12:12:08 +0530

    created redis cluster

---
+++ redis-cluster/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    region     = "${var.region}"
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+}
+
+#create a ElastiCache Redis Cluster
+resource "aws_elasticache_cluster" "redis_db" {
+  cluster_id           = "redis-db"
+  engine               = "redis"
+  node_type            = "cache.m4.large"
+  num_cache_nodes      = 1
+  engine_version       = "7.1.0"
+  port                 = 6379
+  tags = {
+    Name = "myrediscluster"
+  }
+}
---
+++ redis-cluster/output.tf
@@ -0,0 +1,4 @@
+
+output "elasticache_cluster_endpoint" {
+  value       = aws_elasticache_cluster.redisdb.arn
+}
