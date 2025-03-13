5433c22bac038a6ee3c01d41372992356257aac8
Navneet Shahi <navneetshahi345@gmail.com>
2025-03-13 20:54:15 +0530

    created ecr

---
+++ ecr/main.tf
@@ -0,0 +1,12 @@
+
+resource "aws_ecr_repository" "example_ecr_repo" {
+  name = var.ecr_repo_name
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+output "ecr_repo_url" {
+  value = aws_ecr_repository.example_ecr_repo.repository_url
+}
